314aab5ee56344b3613fbc1cbbb65a454a1ba455
fastiz <fastiz@itba.edu.ar>
2023-03-12 12:33:04 +0100

    move parameters into the sample

--- src/main/kotlin/com/example/zookeepertest/ApplicationRunnerImpl.kt
+++ src/main/kotlin/com/example/zookeepertest/ApplicationRunnerImpl.kt
@@ -1,7 +1,6 @@
 package com.example.zookeepertest
 
 import com.example.zookeepertest.sample.barrier.BarrierSample
-import com.example.zookeepertest.sample.barrier.BarrierSampleRunMode
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.stereotype.Component
@@ -11,18 +10,6 @@ class ApplicationRunnerImpl(
     val barrierSample: BarrierSample
 ) : ApplicationRunner {
     override fun run(args: ApplicationArguments) {
-        val (clientName, runnerModeDto) = args.sourceArgs
-
-        val runnerMode = when (runnerModeDto) {
-            "open" -> BarrierSampleRunMode.Open
-            "close" -> BarrierSampleRunMode.Close
-            "enter" -> BarrierSampleRunMode.Enter
-            else -> throw IllegalArgumentException("Invalid runner mode $runnerModeDto")
-        }
-
-        barrierSample.run(
-            clientName = clientName,
-            barrierSampleRunMode = runnerMode
-        )
+        barrierSample.run(args.sourceArgs.toList())
     }
 }
--- src/main/kotlin/com/example/zookeepertest/sample/barrier/BarrierSample.kt
+++ src/main/kotlin/com/example/zookeepertest/sample/barrier/BarrierSample.kt
@@ -2,27 +2,31 @@ package com.example.zookeepertest.sample.barrier
 
 import com.example.zookeepertest.concurrency.BarrierFactory
 import com.example.zookeepertest.sample.barrier.runner.BarrierSampleRunnerFactory
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import org.springframework.stereotype.Component
-import java.lang.Thread.sleep
 
 @Component
 class BarrierSample(
     val barrierFactory: BarrierFactory,
     val barrierSampleRunnerFactory: BarrierSampleRunnerFactory,
 ) {
-    fun run(clientName: String, barrierSampleRunMode: BarrierSampleRunMode) {
+    fun run(args: List<String>) {
+        val (clientName, runnerModeDto) = args
+
+        val runnerMode = when (runnerModeDto) {
+            "open" -> BarrierSampleRunMode.Open
+            "close" -> BarrierSampleRunMode.Close
+            "enter" -> BarrierSampleRunMode.Enter
+            else -> throw IllegalArgumentException("Invalid runner mode $runnerModeDto")
+        }
+
         val barrier = barrierFactory.create("/BarrierSampleDispatcher::barrier")
         val runner = barrierSampleRunnerFactory.create(
-            barrierSampleRunMode = barrierSampleRunMode
+            barrierSampleRunMode = runnerMode
         )
+
         runBlocking {
-            withContext(Dispatchers.IO) {
-                sleep(1000)
-                runner.run(clientName = clientName, barrier = barrier)
-            }
+            runner.run(clientName = clientName, barrier = barrier)
         }
     }
 }
