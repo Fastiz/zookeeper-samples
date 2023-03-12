7a1dcb59f8faeb3425996aa880e83acabf901bf1
fastiz <fastiz@itba.edu.ar>
2023-03-12 09:08:59 +0100

    init

---
+++ src/main/kotlin/com/example/zookeepertest/ZookeepertestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.zookeepertest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZookeepertestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZookeepertestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/zookeepertest/ZookeepertestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.zookeepertest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZookeepertestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
